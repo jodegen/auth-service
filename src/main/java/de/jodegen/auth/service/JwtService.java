00e783e274168414051dff25ab4b406bb32c12ec
jodegen <jonasdegen01@gmail.com>
2025-05-16 13:33:05 +0200

    debug

--- src/main/java/de/jodegen/auth/service/JwtService.java
+++ src/main/java/de/jodegen/auth/service/JwtService.java
@@ -4,6 +4,7 @@ import de.jodegen.auth.model.UserAccount;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class JwtService {
@@ -22,6 +24,8 @@ public class JwtService {
 
     public JwtService(@Value("${jwt.secret}") String secret) {
         this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        log.info("SecretKey {}", this.secretKey);
+        log.info("Secret {}", secret);
     }
 
     public String generateToken(UserAccount user) {
